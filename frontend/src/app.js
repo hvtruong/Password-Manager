b90aa4902ba73cfd0ada352daa217279f2f79e3a
Viet <truonghoangvietthv@gmail.com>
2024-06-03 23:28:53 -0700

    Completed middlewares

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,28 +1,20 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 
+const path = require('path');
 const backendPath = path.join(__dirname, '..', '..', 'backend');
 
 // Setup middlewares
 // Requests logger
 const { logger } = require(path.join(backendPath, 'middleware', 'logger'));
+
 // Error logger
 const errorLogger = require(path.join(backendPath, 'middleware', 'errorHandler'));
 
 // Cross-origin resource sharing
 const cors = require('cors');
 const corsOptions = require(path.join(backendPath, 'config', 'corsOptions'));
-
-// Connect MongoDB
-const connectDB = require(path.join(backendPath, 'config', 'DBConnection'));
-const mongoose = require('mongoose')
-
-// Passport for authentication
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const session = require('express-session');
 
 // Setup all routers
 const homeRouter = require(path.join(backendPath, 'routes', 'home'));
@@ -31,7 +23,6 @@ const registerRouter = require(path.join(backendPath, 'routes', 'register'));
 const authenticateRouter = require(path.join(backendPath, 'routes', 'authenticate'));
 const dashBoardRouter = require(path.join(backendPath, 'routes', 'dashboard'));;
 
-const bodyParser= require('body-parser');
 const app = express();
 
 // Setup view engine
@@ -41,23 +32,15 @@ app.set('view engine', 'jade');
 app.use(logger);
 app.use(cors(corsOptions));
 
-connectDB();
-
-// Use session middleware
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Initialize Passport
-app.use(passport.initialize());
-app.use(passport.session());
-
+// Built-in middleware to parses incoming requests with JSON payloads
 app.use(express.json());
-app.use(bodyParser.json())
+
+// Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+
+// Static middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -75,11 +58,11 @@ app.use(function(req, res, next) {
 
 // Handle errors
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error', {title: '404 Not Found'});
 });
