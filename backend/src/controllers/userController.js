452393a4403e04efb60bb25e23375545076b6618
Viet <truonghoangvietthv@gmail.com>
2024-06-07 03:13:20 -0700

    Refactored user controller and register router

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,16 +1,41 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
-const fetch = require('node-fetch');
-const fs = require('fs');
+
+// Connect to MongoDB
+const connectDB = require('../config/DBConnection');
+connectDB();
+
+// Load schema for user data
+var userModel = require("../models/User");
 
 // @desc Post a new password
 // @route POST /dashboard
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
     try {
-        console.log("ABC");
+        const {username, password, repeatedPassword, emailAddress} = req.body;
+
+        const duplicate = await userModel.findOne({username: req.body.username}).lean().exec();
+
+        if (duplicate) {
+            return res.status(409).json({message: 'Duplicate username'})
+        }
+
+        // Hash password 
+        const hashedPassword = await bcrypt.hash(password, 10) // salt rounds
+
+        const userData = {username, hashedPassword, emailAddress, 'registered': false};
+
+        // Create and store new user 
+        const user = await userModel.create(userData)
+
+        if (user) {
+            res.status(201).json({ message: `New user ${username} created` })
+        } else {
+            res.status(400).json({ message: 'Invalid user data received' })
+        }
     } catch (error) {
-        res.status(500).json({message: "Something went wrong"});
+        res.status(500).json({message: 'Something went wrong'});
     }
 })
 
--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const connectDB = require('../config/DBConnection');
-connectDB();
-
-var userModel = require("../models/User");
-
 /* GET Register page. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
 router.post('/', async function(req, res, next) {
-    const data = {
-      username:req.body.username,
-      password:req.body.password,
-      emailAddress:req.body.emailAddress
-    }
 
     const checking = await userModel.findOne({username: req.body.username})
     try{
